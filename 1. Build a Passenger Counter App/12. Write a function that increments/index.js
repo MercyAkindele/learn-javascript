66b463b4dfa621069855a6cbe20514d993361b21
MercyAkindele <71142767+MercyAkindele@users.noreply.github.com>
2022-03-02 14:03:20 -0600

    Update index.js
    Created a function that adds 1 to a global variable. I then invoked the function 3 times and logged the global variable to see what changes were made

--- 1. Build a Passenger Counter App/12. Write a function that increments/index.js
+++ 1. Build a Passenger Counter App/12. Write a function that increments/index.js
@@ -2,8 +2,11 @@ let lapsCompleted = 0
 
 // Create a function that increments the lapsCompleted variable with one
 // Run it three times
-
-
-
-
+console.log(lapsCompleted)
+function incrementByOne(){
+  lapsCompleted += 1
+}
+incrementByOne()
+incrementByOne()
+incrementByOne()
 console.log(lapsCompleted)
