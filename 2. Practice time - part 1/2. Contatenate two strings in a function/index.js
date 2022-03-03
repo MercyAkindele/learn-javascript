573eaa7727c183be4470492e57c55b73fbe463fa
MercyAkindele <71142767+MercyAkindele@users.noreply.github.com>
2022-03-03 14:33:49 -0600

    Update index.js
    created a function that concatenates different strings and when called gives a full sentence

--- 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
+++ 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
@@ -2,4 +2,8 @@ let name = "Linda"
 let greeting = "Hi there"
 
 // Create a function that logs out "Hi there, Linda!" when called
-
+function welcome(){
+  let hello =  greeting + ", " + name + "!"
+  console.log(hello)
+}
+welcome()
