d66d0e6c8957c6c6afa24f89fe1bf90f0d5e0b25
MercyAkindele <71142767+MercyAkindele@users.noreply.github.com>
2022-03-03 13:20:19 -0600

    Update index.js
    created two variables and then concatenated the two variables in a 3rd variable and logged the results

--- 2. Practice time - part 1/1. Variables practice/index.js
+++ 2. Practice time - part 1/1. Variables practice/index.js
@@ -1,7 +1,11 @@
 // Create two variables, firstName and lastName
+let firstName = "Mercy"
+let lastName = "Akindele"
 
 // Concatenate the two variables into a third variable called fullName
+let fullName = firstName + " " + lastName
 
 // Log fullName to the console
+console.log(fullName)
 
 
