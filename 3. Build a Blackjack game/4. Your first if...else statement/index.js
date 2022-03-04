4e50878c9fe69e3e9fd0c93f233eac77a53ef366
MercyAkindele <71142767+MercyAkindele@users.noreply.github.com>
2022-03-04 16:51:53 -0600

    Update index.js
    If else statements

--- 3. Build a Blackjack game/4. Your first if...else statement/index.js
+++ 3. Build a Blackjack game/4. Your first if...else statement/index.js
@@ -5,4 +5,12 @@ let age = 22
 
 // if less than 21 -> "You can not enter the club!"
 // else            -> "Welcome!"
+let message1 = "Sorry dude, you are too young to get your party on with us!!"
+let message2 = "Alright!!! Come on in!!! Let's get our drinks on!"
 
+if(age < 21){
+  console.log(message1)
+}
+else{
+  console.log(message2)
+}
