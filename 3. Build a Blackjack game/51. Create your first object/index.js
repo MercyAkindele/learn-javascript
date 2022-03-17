b48f1588bbee10b4292506a28ac762b6653dc82a
MercyAkindele <71142767+MercyAkindele@users.noreply.github.com>
2022-03-16 21:16:48 -0500

    Update index.js

--- 3. Build a Blackjack game/51. Create your first object/index.js
+++ 3. Build a Blackjack game/51. Create your first object/index.js
@@ -2,4 +2,12 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
+let castleListing = {
+  twoStory: true,
+  owner: "Mercy",
+  numberOfPeople: 8,
+  amenities: ["pool", "game room", "sauna"]
+}
+console.log(castleListing.numberOfPeople)
+console.log(castleListing.owner)
 
